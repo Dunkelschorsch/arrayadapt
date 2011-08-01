1208a045788e7ca49cdb1e32f6775d2b5038b230
Armin Schmidt <dunkelschorsch@gmail.com>
2011-08-01 14:43:38 +0200

    automatically create header file "mx_traits.hpp"

---
+++ gen_mx_traits.py
@@ -0,0 +1,50 @@
+#!env python
+
+import re
+
+def c_to_mx_typename(c_type, special_map):
+	m = re.search("([a-zA-Z0-9]+)_t", c_type)
+	if m == None:
+		mx_type = c_type
+	else:
+		mx_type = m.groups()[0]
+	if c_type in special_map:
+		mx_type = special_map[c_type]
+	return mx_type.upper()
+
+c_type = ('void', 'double', 'float', 'uint64_t', 'int64_t', 'uint32_t', 'int32_t', 'uint16_t', 'int16_t', 'uint8_t', 'int8_t')
+special_map = {'float': 'single' }
+
+empty_trait = "template <class T>\nstruct mx_traits { };\n\n"
+header_guard = """#ifndef HAVE_MX_TRAITS_HPP
+#define HAVE_MX_TRAITS_HPP
+
+#include <mex.h>
+
+"""
+
+trait_template = """// %s
+template<> struct mx_traits<%s> {
+  static const mxClassID classId = mx%s_CLASS;
+
+  static inline const char* name() {
+    return "%s";
+  }
+};
+
+"""
+
+mx_traits_header = open('mx_traits.hpp', 'wt')
+
+mx_traits_header.write(header_guard)
+mx_traits_header.write(empty_trait)
+
+for type_curr in c_type:
+	for constness in ("", "const ",):
+		full_type = constness + type_curr
+		mx_traits_header.write(trait_template % (full_type, full_type, c_to_mx_typename(type_curr, special_map), full_type))
+
+mx_traits_header.write("#endif\n")
+
+mx_traits_header.close()
+
--- mx_traits.hpp
+++ mx_traits.hpp
@@ -1,81 +1,207 @@
 #ifndef HAVE_MX_TRAITS_HPP
 #define HAVE_MX_TRAITS_HPP
 
-#include "mex.h"
+#include <mex.h>
 
 template <class T>
 struct mx_traits { };
 
-// double
-template<> struct mx_traits<double> {
-    static const mxClassID classId = mxDOUBLE_CLASS;
+// void
+template<> struct mx_traits<void> {
+  static const mxClassID classId = mxVOID_CLASS;
+
+  static inline const char* name() {
+    return "void";
+  }
 };
 
+// const void
+template<> struct mx_traits<const void> {
+  static const mxClassID classId = mxVOID_CLASS;
+
+  static inline const char* name() {
+    return "const void";
+  }
+};
+
+// double
+template<> struct mx_traits<double> {
+  static const mxClassID classId = mxDOUBLE_CLASS;
+
+  static inline const char* name() {
+    return "double";
+  }
+};
+
+// const double
 template<> struct mx_traits<const double> {
-    static const mxClassID classId = mxDOUBLE_CLASS;
+  static const mxClassID classId = mxDOUBLE_CLASS;
+
+  static inline const char* name() {
+    return "const double";
+  }
 };
 
 // float
 template<> struct mx_traits<float> {
-    static const mxClassID classId = mxSINGLE_CLASS;
+  static const mxClassID classId = mxSINGLE_CLASS;
+
+  static inline const char* name() {
+    return "float";
+  }
 };
 
+// const float
 template<> struct mx_traits<const float> {
-    static const mxClassID classId = mxSINGLE_CLASS;
+  static const mxClassID classId = mxSINGLE_CLASS;
+
+  static inline const char* name() {
+    return "const float";
+  }
 };
 
-// uint8
-template<> struct mx_traits<uint8_t> {
-    static const mxClassID classId = mxUINT8_CLASS;
+// uint64_t
+template<> struct mx_traits<uint64_t> {
+  static const mxClassID classId = mxUINT64_CLASS;
+
+  static inline const char* name() {
+    return "uint64_t";
+  }
 };
 
-template<> struct mx_traits<const uint8_t> {
-    static const mxClassID classId = mxUINT8_CLASS;
+// const uint64_t
+template<> struct mx_traits<const uint64_t> {
+  static const mxClassID classId = mxUINT64_CLASS;
+
+  static inline const char* name() {
+    return "const uint64_t";
+  }
 };
 
-// int8
-template<> struct mx_traits<int8_t> {
-    static const mxClassID classId = mxINT8_CLASS;
+// int64_t
+template<> struct mx_traits<int64_t> {
+  static const mxClassID classId = mxINT64_CLASS;
+
+  static inline const char* name() {
+    return "int64_t";
+  }
 };
 
-template<> struct mx_traits<const int8_t> {
-    static const mxClassID classId = mxINT8_CLASS;
+// const int64_t
+template<> struct mx_traits<const int64_t> {
+  static const mxClassID classId = mxINT64_CLASS;
+
+  static inline const char* name() {
+    return "const int64_t";
+  }
 };
 
-// uint16
-template<> struct mx_traits<uint16_t> {
-    static const mxClassID classId = mxUINT16_CLASS;
-};
-
-template<> struct mx_traits<const uint16_t> {
-    static const mxClassID classId = mxUINT16_CLASS;
-};
-
-// int16
-template<> struct mx_traits<int16_t> {
-    static const mxClassID classId = mxINT16_CLASS;
-};
-
-template<> struct mx_traits<const int16_t> {
-    static const mxClassID classId = mxINT16_CLASS;
-};
-
-// uint32
+// uint32_t
 template<> struct mx_traits<uint32_t> {
-    static const mxClassID classId = mxUINT32_CLASS;
+  static const mxClassID classId = mxUINT32_CLASS;
+
+  static inline const char* name() {
+    return "uint32_t";
+  }
 };
 
+// const uint32_t
 template<> struct mx_traits<const uint32_t> {
-    static const mxClassID classId = mxUINT32_CLASS;
+  static const mxClassID classId = mxUINT32_CLASS;
+
+  static inline const char* name() {
+    return "const uint32_t";
+  }
 };
 
-// int32
+// int32_t
 template<> struct mx_traits<int32_t> {
-    static const mxClassID classId = mxINT32_CLASS;
+  static const mxClassID classId = mxINT32_CLASS;
+
+  static inline const char* name() {
+    return "int32_t";
+  }
 };
 
+// const int32_t
 template<> struct mx_traits<const int32_t> {
-    static const mxClassID classId = mxINT32_CLASS;
+  static const mxClassID classId = mxINT32_CLASS;
+
+  static inline const char* name() {
+    return "const int32_t";
+  }
+};
+
+// uint16_t
+template<> struct mx_traits<uint16_t> {
+  static const mxClassID classId = mxUINT16_CLASS;
+
+  static inline const char* name() {
+    return "uint16_t";
+  }
+};
+
+// const uint16_t
+template<> struct mx_traits<const uint16_t> {
+  static const mxClassID classId = mxUINT16_CLASS;
+
+  static inline const char* name() {
+    return "const uint16_t";
+  }
+};
+
+// int16_t
+template<> struct mx_traits<int16_t> {
+  static const mxClassID classId = mxINT16_CLASS;
+
+  static inline const char* name() {
+    return "int16_t";
+  }
+};
+
+// const int16_t
+template<> struct mx_traits<const int16_t> {
+  static const mxClassID classId = mxINT16_CLASS;
+
+  static inline const char* name() {
+    return "const int16_t";
+  }
+};
+
+// uint8_t
+template<> struct mx_traits<uint8_t> {
+  static const mxClassID classId = mxUINT8_CLASS;
+
+  static inline const char* name() {
+    return "uint8_t";
+  }
+};
+
+// const uint8_t
+template<> struct mx_traits<const uint8_t> {
+  static const mxClassID classId = mxUINT8_CLASS;
+
+  static inline const char* name() {
+    return "const uint8_t";
+  }
+};
+
+// int8_t
+template<> struct mx_traits<int8_t> {
+  static const mxClassID classId = mxINT8_CLASS;
+
+  static inline const char* name() {
+    return "int8_t";
+  }
+};
+
+// const int8_t
+template<> struct mx_traits<const int8_t> {
+  static const mxClassID classId = mxINT8_CLASS;
+
+  static inline const char* name() {
+    return "const int8_t";
+  }
 };
 
 #endif
